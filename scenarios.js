8f28f71369c150c234b575fa8a117276d29e0f10
Nick Vincent <nickmvincent@gmail.com>
2025-02-04 16:10:55 -0800

    small data rework (no related_inputs, add phrase_for_card)

--- scenarios.js
+++ scenarios.js
@@ -1,6 +1,6 @@
 export const scenariosData = [
     {
-        title: "Distributing AI Revenue",
+        title: "Distributing AI Company Revenue Broadly",
         description: "If we distribute AI revenue (say, {yearly_revenue__openai__dollars}) to some group of people (say, {group_size__world__people}), how much will each person get?",
         inputs: ["yearly_revenue__openai__dollars", "group_size__world__people"],
         calculate: (yearlyRevenue, population) => yearlyRevenue / population,
@@ -11,7 +11,18 @@ export const scenariosData = [
         }
     },
     {
-        title: "Commissioning an LLM Dataset Cost Estimation",
+        title: "Distributing Money from Data Deals",
+        description: "If we distribute the payments from recent data deal (say, {deal_value__reddit_google__dollars}) to some group of people (say, {deal_group_size__reddit__daily_active_users}), how much will each person get?",
+        inputs: ["deal_value__reddit_google__dollars", "deal_group_size__reddit__daily_active_users"],
+        calculate: (dealValue, dailyActiveUsers) => dealValue / dailyActiveUsers,
+        unitDetails: "dollars / people",
+        result: {
+            label: "Per Person Revenue",
+            units: "dollars"
+        }
+    },
+    {
+        title: "Commissioning New Datasets",
         description: "How much would it cost to pay for a brand new LLM-scale pre-training dataset (say, {dataset_size__llama3__tokens}) assuming moderate freelance writing wages (say, {wage_data__generic_freelance_higher__dollars_per_word})?",
         inputs: ["dataset_size__llama3__tokens", "training_detail__openai__words_per_token", "wage_data__generic_freelance_higher__dollars_per_word"],
         calculate: (tokens, wordsPerToken, freelanceRate) => tokens * wordsPerToken * freelanceRate,
@@ -20,26 +31,7 @@ export const scenariosData = [
             units: "dollars",
         }
     },
-    {
-        title: "Revenue and tokens",
-        description: "Estimate the revenue generated per token in the dataset.",
-        inputs: ["yearly_revenue__openai__dollars", "dataset_size__llama3__tokens"],
-        calculate: (yearlyRevenue, totalTokens) => yearlyRevenue / totalTokens,
-        result: {
-            label: "Revenue Per Token",
-            units: "dollars",
-        }
-    },
-    {
-        title: "Number of Contributions",
-        description: "Calculate the number of distinct contributions based on tokens.",
-        inputs: ["dataset_size__llama3__tokens", "dataset_attribute__redpajama__tokens_per_contribution"],
-        calculate: (totalTokens, tokensPerContribution) => totalTokens / tokensPerContribution,
-        result: {
-            label: "Contributions",
-            units: "documents",
-        }
-    },
+    
     {
         title: "Revenue Per Contribution",
         description: "Estimate the revenue generated per contribution based on tokens.",
@@ -68,15 +60,7 @@ export const scenariosData = [
             label: "Coverage Ratio",
         }
     },
-    {
-        title: "Value Per Reddit User",
-        description: "Estimate the value generated per Reddit user based on the deal value.",
-        inputs: ["deal_value__reddit_google__dollars", "group_size_users__reddit__daily_active_users"],
-        calculate: (dealValue, dailyActiveUsers) => dealValue / dailyActiveUsers,
-        result: {
-            label: "Value Per User",
-        }
-    },
+    
     {
         title: "Freelance Cost Per Book",
         description: "Calculate the cost of commissioning a book based on freelance rates.",
@@ -95,13 +79,34 @@ export const scenariosData = [
             label: "Total Freelance Cost",
         }
     },
+    // {
+    //     title: "Revenue Per WSJ Journalist",
+    //     description: "Estimate the revenue generated per WSJ journalist based on the deal value.",
+    //     inputs: ["deal_value__newscorp__dollars", "group_size__wsj__journalists"],
+    //     calculate: (dealValue, numberOfJournalists) => dealValue / numberOfJournalists,
+    //     result: {
+    //         label: "Revenue Per Journalist",
+    //     }
+    // },
+
     {
-        title: "Revenue Per WSJ Journalist",
-        description: "Estimate the revenue generated per WSJ journalist based on the deal value.",
-        inputs: ["deal_value__newscorp__dollars", "group_size__wsj__journalists"],
-        calculate: (dealValue, numberOfJournalists) => dealValue / numberOfJournalists,
+        title: "For reference: Revenue and tokens",
+        description: "Estimate the revenue generated per token in the dataset (divide {yearly_revenue__openai__dollars} by {dataset_size__llama3__tokens}).",
+        inputs: ["yearly_revenue__openai__dollars", "dataset_size__llama3__tokens"],
+        calculate: (yearlyRevenue, totalTokens) => yearlyRevenue / totalTokens,
         result: {
-            label: "Revenue Per Journalist",
+            label: "Revenue Per Token",
+            units: "dollars",
         }
-    }
+    },
+    {
+        title: "For reference: Number of Contributions in a 'Dataset'",
+        description: "Calculate the number of distinct contributions based on tokens.",
+        inputs: ["dataset_size__llama3__tokens", "dataset_attribute__redpajama__tokens_per_contribution"],
+        calculate: (totalTokens, tokensPerContribution) => totalTokens / tokensPerContribution,
+        result: {
+            label: "Contributions",
+            units: "documents",
+        }
+    },
 ];
